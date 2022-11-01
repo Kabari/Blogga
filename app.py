8c2aaa32ef44b7e02f5cf09a6d42bd9981b87250
Emmanuel Kabari <kabariirenaeus@gmail.com>
2022-11-01 09:49:22 +0100

    created edit_post, post, contact route and their respective html pages

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template, request, url_for, redirect
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask_login import LoginManager, UserMixin, login_user, logout_user, login_required
-from datetime import datetime
+from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from datetime import date, datetime
 import os
 
 base_dir = os.path.dirname(os.path.realpath(__file__))
@@ -24,12 +24,12 @@ login_manager = LoginManager(app)
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
     id = db.Column(db.Integer(), primary_key=True)
-    firstname = db.Column(db.String, nullable=False)
-    lastname = db.Column(db.String, nullable=False)
+    firstname = db.Column(db.String(), nullable=False, unique=False)
+    lastname = db.Column(db.String(), nullable=False, unique=False)
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(60), nullable=False, unique=True)
     password_hash = db.Column(db.Text(), nullable=False)
-    post = db.relationship('BlogPost', backref='owned_user', lazy=True)
+    posts = db.relationship('BlogPost', backref='owner')
 
     def __repr__(self):
         return f"User <{self.username}>"
@@ -40,11 +40,12 @@ class BlogPost(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
     caption = db.Column(db.String(), nullable=False)
     content = db.Column(db.String(), nullable=False)
-    posted_by = db.Column(db.Integer(), db.ForeignKey('user.username'))
-    date_created = db.Column(db.DateTime(), default=datetime.utcnow)
+    owner_id = db.Column(db.Integer(), db.ForeignKey(
+        'users.id'), nullable=False)
+    date_created = db.Column(db.DateTime(), default=date.month)
 
     def __repr__(self):
-        return f"BlogPost <{self.picture}>, <{self.caption}>, <{self.content}>, <{self.date_created}>"
+        return f"BlogPost <{self.caption}, {self.content}, {self.date_created}, {self.owner_id}>"
 
 
 @login_manager.user_loader
@@ -57,10 +58,7 @@ def user_loader(id):
 def home():
     posts = BlogPost.query.all()
 
-    context = {
-        'posts':posts
-    }
-    return render_template('index.html', **context)
+    return render_template('index.html', posts=posts)
 
 # @app.route('/')
 # def get_all_posts():
@@ -75,7 +73,7 @@ def login():
 
     if user and check_password_hash(user.password_hash, password):
         login_user(user)
-        return redirect(url_for('index'))
+        return redirect(url_for('home'))
 
     return render_template('login.html')
 
@@ -83,6 +81,8 @@ def login():
 @app.route('/signup', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
+        firstname = request.form.get('firstname')
+        lastname = request.form.get('lastname')
         username = request.form.get('username')
         email = request.form.get('email')
         password = request.form.get('password')
@@ -98,7 +98,7 @@ def register():
 
         password_hash = generate_password_hash(password)
 
-        new_user = User(username=username, email=email,
+        new_user = User(firstname=firstname, lastname=lastname, username=username, email=email,
                         password_hash=password_hash)
         db.session.add(new_user)
         db.session.commit()
@@ -111,11 +111,11 @@ def register():
 @app.route('/logout')
 def logout():
     logout_user()
-    return redirect(url_for('index'))
+    return redirect(url_for('home'))
 
 
-
-@app.route('/create_post', methods=['POST'])
+@app.route('/create', methods=['GET', 'POST'])
+@login_required
 def create_post():
     # username = request.form.get('username')
     # email = request.form.get('email')
@@ -125,37 +125,54 @@ def create_post():
         caption = request.form.get('caption')
         content = request.form.get('content')
 
-        new_post = BlogPost(caption=caption, content=content)
+        owner = current_user.id
+        new_post = BlogPost(caption=caption, content=content, owner_id=owner)
 
         db.session.add(new_post)
-        db.session.commit
+        db.session.commit()
 
         return redirect(url_for('home'))
-    
-    return render_template('create_post.html')
+
+    return render_template('create.html')
 
 
-@app.route('/edit_post/<int:id>', methods=['GET', 'POST'])
+@app.route('/blogposts/post/<int:id>/edit_post', methods=['GET', 'POST'])
+@login_required
 def edit_post(id):
     post_to_edit = BlogPost.query.get_or_404(id)
+    # post =
 
     if request.method == 'POST':
         post_to_edit.caption = request.form.get('caption')
         post_to_edit.content = request.form.get('content')
 
+        db.session.add(post_to_edit)
         db.session.commit()
-        return redirect(url_for('home'))
+        return redirect(url_for('view_post', id=id))
 
-    context = {
-        'blog_post': post_to_edit
-    }
+    if current_user.id == post_to_edit.owner_id:
+        post_to_edit.caption = post_to_edit.caption
+        post_to_edit.content = post_to_edit.content
+        return render_template('edit_post.html', post=post_to_edit)
 
-    return render_template('edit_post.html', **context)
+    else:
+        post = BlogPost.query.get_or_404(id)
+
+        return render_template('post.html', post=post)
 
 
+@app.route('/blogposts/post/<int:id>', methods=['GET', 'POST'])
+def view_post(id):
+    post = BlogPost.query.get_or_404(id)
 
+    return render_template('post.html', post=post)
+
+
+@app.route('/contact')
+def contact():
+
+    return render_template('contact.html')
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-
