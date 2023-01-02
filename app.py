694d042f0eefecdd561fe7097fe34812b19d204b
Emmanuel Kabari <kabariirenaeus@gmail.com>
2023-01-02 13:50:51 +0100

    Added wtforms to all forms and ckeditor

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import desc
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import LoginManager, UserMixin, current_user, login_user, logout_user, login_required
+from forms import CreatePostForm, LoginForm, RegisterForm
+from flask_ckeditor import CKEditor
 from datetime import datetime
 import os
 
@@ -16,7 +18,10 @@ app.config["SQLALCHEMY_DATABASE_URI"] = 'sqlite:///' + \
     os.path.join(base_dir, 'blogga.db')
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 app.config["SECRET_KEY"] = '368a9fb9cbe27df3ac61bd23'
+app.config['CKEDITOR_PKG_TYPE'] = 'standard'
 
+
+ckeditor = CKEditor(app)
 db.init_app(app)
 
 login_manager = LoginManager(app)
@@ -30,7 +35,7 @@ class User(db.Model, UserMixin):
     username = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(60), nullable=False, unique=True)
     password_hash = db.Column(db.Text(), nullable=False)
-    posts = db.relationship('BlogPost', backref='owner')
+    posts = db.relationship('BlogPost', backref='owner', lazy='dynamic')
 
     def __repr__(self):
         return f"User <{self.username}>"
@@ -73,25 +78,28 @@ def home():
 # login route
 @app.route('/login', methods=['GET', 'POST'])
 def login():
-    username = request.form.get('username')
-    password = request.form.get('password')
+    form = LoginForm()
+    if request.method == 'POST':
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-    user = User.query.filter_by(username=username).first()
-    if user and check_password_hash(user.password_hash, password):
-        login_user(user)
-        flash("Login successful")
-        return redirect(url_for('home'))
-    elif user == None and password == None:
-        flash('Enter your login credentials')
-    else:
-        flash("User credentials not correct!!")
+        user = User.query.filter_by(username=username).first()
+        if user and check_password_hash(user.password_hash, password):
+            login_user(user)
+            flash("Login successful")
+            return redirect(url_for('home'))
+        elif user == None and password == None:
+            flash('Enter your login credentials')
+        else:
+            flash("User credentials not correct!!")
 
-    return render_template('login.html')
+    return render_template('login.html', form=form)
 
 
 # register route
 @app.route('/signup', methods=['GET', 'POST'])
 def register():
+    form = RegisterForm()
     if request.method == 'POST':
         firstname = request.form.get('firstname')
         lastname = request.form.get('lastname')
@@ -125,7 +133,7 @@ def register():
         flash("Registration Successfull")
         return redirect(url_for('login'))
 
-    return render_template('signup.html')
+    return render_template('signup.html', form=form)
 
 
 # logout route to logout a user
@@ -140,12 +148,12 @@ def logout():
 @app.route('/create', methods=['GET', 'POST'])
 @login_required
 def create_post():
-    if request.method == 'POST':
-        caption = request.form.get('caption')
-        content = request.form.get('content')
+    form = CreatePostForm()
 
+    if form.validate_on_submit():
         owner = current_user.id
-        new_post = BlogPost(caption=caption, content=content, owner_id=owner)
+        new_post = BlogPost(caption=form.caption.data,
+                            content=form.content.data, owner_id=owner)
 
         db.session.add(new_post)
         db.session.commit()
@@ -154,35 +162,50 @@ def create_post():
 
         return redirect(url_for('home'))
 
-    return render_template('create.html')
+    return render_template('create.html', form=form, owner=current_user)
 
 
 # edit post route, only the owner of the post can edit the post
 @app.route('/blogposts/post/edit_post/<int:id>/', methods=['GET', 'POST'])
 @login_required
 def edit_post(id):
-    post_to_edit = BlogPost.query.get_or_404(id)
+    # post_to_edit = BlogPost.query.get_or_404(id)
 
-    if request.method == 'POST':
-        post_to_edit.caption = request.form.get('caption')
-        post_to_edit.content = request.form.get('content')
+    # if request.method == 'POST':
+    #     post_to_edit.caption = request.form.get('caption')
+    #     post_to_edit.content = request.form.get('content')
 
-        db.session.add(post_to_edit)
+    #     db.session.add(post_to_edit)
+    #     db.session.commit()
+
+    #     flash("Post Updated!")
+    #     return redirect(url_for('view_post', id=id))
+
+    # if current_user.id == post_to_edit.owner_id:
+    #     post_to_edit.caption = post_to_edit.caption
+    #     post_to_edit.content = post_to_edit.content
+    #     return render_template('edit_post.html', post=post_to_edit)
+
+    # else:
+    #     flash("You do not have the previledge to edit this post")
+    #     post = BlogPost.query.get_or_404(id)
+
+    #     return render_template('post.html', post=post)
+
+    post = BlogPost.query.get_or_404(id)
+    form = CreatePostForm(caption=post.caption,
+                               content=post.content, owner=current_user)
+
+    if form.validate_on_submit():
+        post.caption = form.caption.data
+        post.content = form.content.data
+
+        db.session.add(post)
         db.session.commit()
 
         flash("Post Updated!")
         return redirect(url_for('view_post', id=id))
-
-    if current_user.id == post_to_edit.owner_id:
-        post_to_edit.caption = post_to_edit.caption
-        post_to_edit.content = post_to_edit.content
-        return render_template('edit_post.html', post=post_to_edit)
-
-    else:
-        flash("You do not have the previledge to edit this post")
-        post = BlogPost.query.get_or_404(id)
-
-        return render_template('post.html', post=post)
+    return render_template('edit_post.html', post=post, form=form)
 
 
 # single blog post view route
@@ -226,6 +249,13 @@ def about():
     return render_template('about.html')
 
 # Dashboard page
-@app.route('/dashboard')
-def dashboard():
-    return render_template('dashboard.html')
+
+
+@app.route("/dashboard/<int:id>")
+@login_required
+def dashboard(id):
+    user = User.query.filter_by(id=id).first()
+    if current_user.id == user.id:
+        users_id = user.id
+        posts = BlogPost.query.filter_by(owner_id=users_id).all()
+    return render_template("dashboard.html", posts=posts)
